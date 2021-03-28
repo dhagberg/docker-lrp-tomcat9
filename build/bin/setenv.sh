605a027c0c2976e8c5fe63f66f00be89a5da1a98
D. J. Hagberg <dhagberg@millibits.com>
2021-03-28 19:52:20 +0000

    Log via log4j 2.x - both Tomcat and deployed webapps.  Add dependencies so that slf4j, log4j 1.x, commons-logging, and jboss-logging all go through Log4j 2.x logger/formatter/rolling file appender.

---
+++ build/bin/setenv.sh
@@ -0,0 +1,13 @@
+# Need to specify log4j2 config file using system property
+CATALINA_OPTS="${CATALINA_OPTS} -Dlog4j2.configurationFile=$CATALINA_HOME/log4j2/log4j2-tomcat.xml"
+
+# Override JUL Log Manager to use log4j2
+LOGGING_MANAGER="-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager"
+
+# Log4j2 jars need to be in system classpath
+LOG4J2_JARS=$(find $CATALINA_HOME/log4j2 -type f -name '*.jar' -print | tr '\n' ':' | sed -e 's/:$//')
+if [ -z "$CLASSPATH" ]; then
+    CLASSPATH="$LOG4J2_JARS"
+else
+    CLASSPATH="${CLASSPATH}:$LOG4J2_JARS"
+fi
