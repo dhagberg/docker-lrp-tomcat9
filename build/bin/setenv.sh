a5ff478d32ecb27ec8b3f972314b38e7bc82de20
D. J. Hagberg <dhagberg@millibits.com>
2021-12-10 18:45:31 +0000

    Close RCE security hole in log4j2 https://www.lunasec.io/docs/blog/log4j-zero-day/ Log4Shell CVE-2021-44228

--- build/bin/setenv.sh
+++ build/bin/setenv.sh
@@ -1,6 +1,9 @@
 # Need to specify log4j2 config file using system property
 CATALINA_OPTS="${CATALINA_OPTS} -Dlog4j2.configurationFile=$CATALINA_HOME/log4j2/log4j2-tomcat.xml"
 
+# https://www.lunasec.io/docs/blog/log4j-zero-day/ Log4Shell CVE-2021-44228:
+CATALINA_OPTS="${CATALINA_OPTS} -Dlog4j2.formatMsgNoLookups=true"
+
 # Override JUL Log Manager to use log4j2
 LOGGING_MANAGER="-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager"
 
