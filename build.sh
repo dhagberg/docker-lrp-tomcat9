2524e0ebf180f54457df3a747ad0bf01770a26f8
D. J. Hagberg <dhagberg@millibits.com>
2024-09-06 04:27:37 +0000

    Switch to ubuntu 24.04 base from CentOS, apt/deb instead of yum/rpm, latest tomcat, latest log4j, use -u- rename to be able to deploy alongside existing CentOS images

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -xe
-PLAIN_NAME=lrp-tomcat9
+PLAIN_NAME=lrp-u-tomcat9
 HUB_NAME=dhagberg/$PLAIN_NAME
 CI_NAME=cga-ci:5000/$PLAIN_NAME
 
 docker build --pull -t $HUB_NAME .
 
 # Tag and push if given
-if [ "$1" = "push" ]; then
+if [ $# -ge 1 ]; then
+  if [ "$1" = "push" ]; then
     docker push $HUB_NAME
     docker tag  $HUB_NAME $CI_NAME
     docker push $CI_NAME
+  fi
 fi
